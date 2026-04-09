311d153c2aa16d227f73f6555da954dc44de7db5
Danielpalma54 <danielpalma212002@gmail.com>
2026-04-09 10:51:41 -0500

    Delete mensaje

--- App1.5/src/app/components/todo/todo.ts
+++ App1.5/src/app/components/todo/todo.ts
@@ -10,7 +10,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-todo',
   standalone: true, 
-  imports: [CommonModule, RouterModule, TodoListComponent, FormsModule],              //IMPORTANTE
+  imports: [CommonModule, RouterModule, TodoListComponent, FormsModule],              
   templateUrl: './todo.html',
 })
 export class TodoComponent implements OnInit {                             
